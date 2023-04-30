1b5e87aac68cb3caef59151d94e853fc21d4e413
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-04-30 04:09:37 -0500

    ayy

--- public/build/index.js
+++ public/build/index.js
@@ -1,1 +1,35 @@
 "use strict";
+const mousePosition = { mouseX: 0, mouseY: 0 };
+const lastPosition = { ...mousePosition };
+let timesRun = 0;
+let timer = 1000;
+//detect mouseposition
+document.addEventListener("mousemove", handleMouse);
+function handleMouse(event) {
+    const { clientX, clientY } = event;
+    mousePosition.mouseX = clientX;
+    mousePosition.mouseY = clientY;
+}
+let workDiv = document.querySelector(".workingDiv");
+startInterval();
+function startInterval() {
+    const interval = setInterval(() => {
+        if (lastPosition.mouseX === mousePosition.mouseX && lastPosition.mouseY === mousePosition.mouseY) {
+            return;
+        }
+        if (timesRun >= 10) {
+            timesRun = 0;
+            if (timer > 100) {
+                timer -= 100;
+                clearInterval(interval);
+                startInterval();
+            }
+            workDiv.innerHTML = "";
+            return;
+        }
+        lastPosition.mouseX = mousePosition.mouseX;
+        lastPosition.mouseY = mousePosition.mouseY;
+        workDiv.innerHTML += `<p>X: ${mousePosition.mouseX} Y: ${mousePosition.mouseX}</p>`;
+        timesRun++;
+    }, timer);
+}
