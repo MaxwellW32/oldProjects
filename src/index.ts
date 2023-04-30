5db76141f3b35d570f9db2b2a55de0f44a9a8c6d
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-04-30 02:49:29 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+//load navbar
+document.addEventListener("DOMContentLoaded", function() {
+    function getNavBar(){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'menus/navbar.html', true);
+        xhr.onreadystatechange = function() {
+            if (this.readyState !== 4) return;
+            if (this.status !== 200) return; // or whatever error handling you want
+            const myNav = document.querySelector('.holdNavBar') as HTMLElement
+            myNav.innerHTML = this.responseText;
+        };
+        xhr.send();
+    }
+
+    function getFooter(){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'menus/footer.html', true);
+        xhr.onreadystatechange = function() {
+            if (this.readyState !== 4) return;
+            if (this.status !== 200) return; // or whatever error handling you want
+            const myNav = document.querySelector('.holdFooter') as HTMLElement
+            myNav.innerHTML = this.responseText;
+        };
+        xhr.send();
+    }
+
+    getNavBar()
+    getFooter()
+});
