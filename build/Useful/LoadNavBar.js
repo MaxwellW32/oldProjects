dc50974c669d9a39582abf3e3866a73d06be0060
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-04-30 03:10:22 -0500

    working

---
+++ build/Useful/LoadNavBar.js
@@ -0,0 +1,32 @@
+"use strict";
+//load navbar
+document.addEventListener("DOMContentLoaded", function () {
+    function getNavBar() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/build/public/menus/navbar.html', true);
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4)
+                return;
+            if (this.status !== 200)
+                return; // or whatever error handling you want
+            const myNav = document.querySelector('.holdNavBar');
+            myNav.innerHTML = this.responseText;
+        };
+        xhr.send();
+    }
+    function getFooter() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/build/public/menus/footer.html', true);
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4)
+                return;
+            if (this.status !== 200)
+                return; // or whatever error handling you want
+            const myNav = document.querySelector('.holdFooter');
+            myNav.innerHTML = this.responseText;
+        };
+        xhr.send();
+    }
+    getNavBar();
+    getFooter();
+});
